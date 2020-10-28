d5212fbbb3b2b1d29b4bfd5768931090e7d2bfcf
Andrzej Drabarek <andrzej.drabarek@protonmail.com>
2020-10-28 11:58:00 +0100

    Simple exercise: displaying two numbers input by the user.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first_number = String::new();
+    let mut second_number = String::new();
+    
+    io::stdin().read_line(&mut first_number).
+      expect("Failed to read line.");
+    let first_number: i32 = first_number.trim().parse().expect("Failed to parse number.");
+
+    io::stdin().read_line(&mut second_number).
+      expect("Failed to read line.");
+    let second_number: i32 = second_number.trim().parse().expect("Failed to parse number.");
+
+    println!("First number: {}, second number: {}", first_number, second_number)
 }
